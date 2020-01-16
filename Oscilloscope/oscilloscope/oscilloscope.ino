3821f63d06069a74f1524d0eae972de6e3d45e4a
duicul <duicul@yahoo.com>
2020-01-16 23:01:53 +0200

    timer1 1sec adc reads interrupt

--- Oscilloscope/oscilloscope/oscilloscope.ino
+++ Oscilloscope/oscilloscope/oscilloscope.ino
@@ -4,11 +4,56 @@
 #include <stdio.h>
 #define USART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) 
-
+#define MAX_READINGS 20
 uint16_t i=0,del_time=100,inc=3;
 uint16_t samples=100;
 uint16_t settings[10][10],setting_ind=0;
+uint16_t readings[100],read_index=0;
+boolean read_on=true;
 
+void timer1init(){
+    cli();
+    TCCR1A &= ~(1<<COM1A1) &   // Clearing bits
+            ~(1<<COM1A0) &
+            ~(1<<COM1B1) &
+            ~(1<<COM1B0) &
+            ~(1<<WGM11) &
+            ~(1<<WGM10);
+
+
+     TCCR1B &= ~(1<<ICNC1) &    // Clearing bits
+            ~(1<<ICES1) &
+            ~(1<<WGM13) &
+            ~(1<<CS11);
+
+     TCCR1B |= (1<<WGM12) |    // Setting bits
+            (1<<CS12) |
+            (1<<CS10);
+
+      OCR1A = 0x3D08;
+
+      // OCIE1A interrupt flag set
+      TIMSK1 |= (1<<OCIE1A);
+
+      // Start counter at 0, not that it would matter much in this case...
+      TCNT1 = 0;
+    sei();
+    // enable interrupts
+}
+
+void adc_init(void)
+{
+    ADMUX = (1<<REFS0);     //select AVCC as reference
+    ADCSRA = (1<<ADEN) | 7;  //enable and prescale = 128 (16MHz/128 = 125kHz)
+}
+
+uint16_t readAdc(char chan)
+{
+    ADMUX = (1<<REFS0) | (chan & 0x0f);  //select input and ref
+    ADCSRA |= (1<<ADSC);                 //start the conversion
+    while (ADCSRA & (1<<ADSC));          //wait for end of conversion
+    return ADCW;
+}
 
 void UART_init()
 { cli();
@@ -43,23 +88,62 @@ void UART_TxChar(char ch)
   UDR0 = ch ;
 }
 
-ISR(USART_RX_vect)//USART_RX_vect
+ISR(USART_RX_vect)//USART_RX_vecta
 { char ReceivedByte;
   ReceivedByte=UDR0;
   UDR0=ReceivedByte;
   PORTB^=(1<<5);
 }
 
+ISR (TIMER1_COMPA_vect){
+  if(read_on)
+    if(read_index>=MAX_READINGS)
+      read_on=false;
+    else{
+      readings[read_index++]=readAdc(0);
+       PORTB^=(1<<5);}
+ 
+}
 
 void setup() {
   DDRB|=(1<<5);
   PORTB&=~(1<<5);
   UART_init();
   //UART_TxChar('2');
+  DDRC|=(1<<0);
+  PORTC&=~(1<<0);
+  timer1init();
+  
+}
+
+void sendint(uint16_t t){
+  uint16_t aux = t;
+  uint8_t ind=0,i;
+  char val[10],c;
+  do{
+    c=aux%10+'0';
+    val[ind++]=c;
+    aux/=10;
+  }while(aux!=0);
+  for(i=0;i<ind;i++)
+     UART_TxChar(val[i]);
 }
 
 void loop() {
-  //UART_TxChar(UART_RxChar());
+  uint16_t i=0;
+  if(!read_on){
+    for(i=0;i<read_index;i++){
+      sendint(readings[i]);
+      UART_TxChar('\n');}
+    read_index=0;
+    read_on=true;}
+    delay(10000);
+  /*
+  PORTC&=~(1<<0);
+  uint16_t readdata=readAdc(0);
+  sendint(readdata);
+  UART_TxChar('\n');
+  delay(1000);*/
   //PORTB^=(1<<5);
   /*PORTB^=(1<<5);
   delay(del_time);
