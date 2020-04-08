6b187050db85ce265ed201df52cef2ac664fbe8c
duicul <duicul@yahoo.com>
2020-04-08 08:52:15 +0300

    faster multithreading + better log formatting (currently not json)

--- filelengthmatch.py
+++ filelengthmatch.py
@@ -3,13 +3,15 @@ import os
 import time
 import traceback
 import threading
+import concurrent.futures
 
 data_log=""
 
-def video_len(path):
+def video_len(path,measure_len):
     #print()
     #print(path)
-    return False
+    if not measure_len:
+        return False
     init=time.time()
     try:
         clip = VideoFileClip(path)
@@ -30,25 +32,16 @@ def video_len(path):
     #print()
     return len_vid
 
-def match(file1,aux_file,ind1,dur1,size1,total):
+def match(file1,aux_file,ind1,dur1,size1,total,measure_len):
+    data_log=""
     no_files=len(aux_file)
     for ind2 in range(ind1+1,no_files):
             perc=ind2/no_files
-            #if ind1==0:
-            #    status=[]
-            #    for i in range(9):
-            #        status.append(False)
-            #    for ia in range(len(status)):
-            #        if not status[ia]:
-            #            if (ia+1)*0.1<perc:
-            #                print("First pass:"+str(ia)+" "+str(perc*100)+"% "+str(status[ia]))
-            #                status[ia]=True
-            #                print(status)
             file2=aux_file[ind2][1]
             if(file1!=file2):
                 try:
                     if len(aux_file[ind2])<= 2:
-                        dur2=video_len(file2)
+                        dur2=video_len(file2,measure_len)
                         aux_file[ind2].append(dur2)
                         total=total+1
                     else:
@@ -58,36 +51,41 @@ def match(file1,aux_file,ind1,dur1,size1,total):
                         aux_file[ind2].append(size2)
                     else:
                         size2=aux_file[ind2][3]
-                    if dur1 == dur2 and dur1!=False and dur2!=False :
-                        data_log=data_log+("\nMatch duration: "+str(dur1)+" "+file1+" "+file2)
-                        print("\nMatch duration: "+str(dur1))
-                        print(file1)
-                        print(file2)
-                        print()
                     if aux_file[ind1][0] == aux_file[ind2][0]:
-                        data_log=data_log+("\nMatch name: "+file1+" "+file2)
+                        data_log=data_log+("\nMatch name: \n"+file1+"\n "+file2)
                         print("\nMatch name: "+aux_file[ind1][1])
                         print(file1)
                         print(file2)
                         print()
-                    if size1 == size2:
-                        data_log=data_log+("\nMatch size: "+str(size1)+" "+file1+" "+file2)
+                    elif size1 == size2 and size1!=False and size2!=False:
+                        data_log=data_log+("\nMatch size: "+str(size1)+"\n "+file1+"\n "+file2)
                         print("\nMatch size: "+str(size1)+" "+file1+" "+file2)
                         print(file1)
                         print(file2)
                         print()
+                    elif dur1 == dur2 and dur1!=False and dur2!=False :
+                        data_log=data_log+("\nMatch duration: "+str(dur1)+"\n "+file1+"\n "+file2)
+                        print("\nMatch duration: "+str(dur1))
+                        print(file1)
+                        print(file2)
+                        print()  
                 except Exception:
                     print(file1+" "+str(dur1))
                     print(file2+" "+str(dur2))
-                    data_log=data_log+("\n"+traceback.format_exc())
                     print(traceback.format_exc())
+                    data_log=data_log+("\n"+traceback.format_exc())
+    return data_log
+                    
 
 def join_threads(thread_list):
+    return_value=""
     for thread in thread_list:
-        thread.join()
-    
+        while not thread.done():
+                pass
+        return_value+= thread.result()
+    return return_value
 
-def parse_dir(paths):
+def parse_dir(paths,max_threads,outputfile,measure_len):
     data_log=""
     init=time.time()
     # List to store all   
@@ -118,6 +116,9 @@ def parse_dir(paths):
     for i in range(9):
         status_tot.append(False)
     for ind1 in range(no_files):
+        for i in range(100):
+            if (int(no_files*((i+1)/100)))==ind1:
+                print(str(i+1)+"%")
         if ind1 == 1:
             print("Finished first pass")
             data_log=data_log+"\nFinished first pass\n"
@@ -131,7 +132,7 @@ def parse_dir(paths):
         dur1=0
         size1=0
         if len(aux_file[ind1])<= 2:
-            dur1=video_len(file1)
+            dur1=video_len(file1,measure_len)
             aux_file[ind1].append(dur1)
             total=total+1
         else:
@@ -141,12 +142,13 @@ def parse_dir(paths):
             aux_file[ind1].append(size1)
         else:
             size1=aux_file[ind1][3]
-        t1 = threading.Thread(target=match, args=(file1,aux_file,ind1,dur1,size1,total)) 
-        t1.start()
-        thread_list.append(t1)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            t1 = executor.submit(match,file1,aux_file,ind1,dur1,size1,total,measure_len)
+        #t1 = threading.Thread(target=match, args=(file1,aux_file,ind1,dur1,size1,total,data_log))
         active_threads+=1
-        if active_threads >= 6 :
-            join_threads(thread_list)
+        thread_list.append(t1)
+        if active_threads >= max_threads :
+            data_log+=join_threads(thread_list)
             active_thread=0
             thread_list=[]
         #match(file2,aux_file,ind1)
@@ -160,7 +162,7 @@ def parse_dir(paths):
     data_log=data_log+"\n"+str(time.time()-init)
     print("Operations: "+str(total))
     print(time.time()-init)
-    f = open("log_file.txt", "w")
+    f = open(outputfile, "w")
     f.write(data_log)
     f.close()
     return L
@@ -180,5 +182,6 @@ def parse_dir(paths):
                     print(file1[1])
                     print(file2[1])
                     print()
-        
-parse_dir(["D:\ding\Site","D:\ding\Star"])
+if __name__ == "__main__":
+    data_log=""
+    parse_dir(["D:\Book"],10,"log_file.txt",False)
