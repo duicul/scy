616f189518685d3cde29500264521d87b5b8dac0
duicul <duicul@yahoo.com>
2020-04-11 07:12:17 +0300

    applied convolution for lower frequency detection and prediction for last date

--- covid-data/coviddata.py
+++ covid-data/coviddata.py
@@ -6,12 +6,114 @@ import requests
 import matplotlib.dates
 import datetime
 import os
+import concurrent.futures
+        
+def draw_data(countries,filename,x,y,xgrow,ygrow,val_type,dates):
+    today=datetime.datetime.today()
+    direcdate=str(today.day)+"_"+str(today.month)+str(val_type)
+    if not os.path.exists(direcdate+"/point"):
+        os.makedirs(direcdate+"/point")
+    if not os.path.exists(direcdate+"/line"):
+        os.makedirs(direcdate+"/line")
+    yconvval=[]
+    fig, ax = plt.subplots()   
+
+    if dates:
+        plt.title("Countries covid data")
+        plt.xlabel("Date")
+        plt.ylabel("Cases")
+        ax.xaxis.set_tick_params(rotation=30, labelsize=10)
+        for i in range(len(x)):
+            print(countries[i])
+            xi=np.array(x[i])
+            yi=np.array(y[i])
+            plt.plot_date(xi,yi,label=countries[i], xdate=True, ydate=False)
+
+        plt.legend()        
+        plt.savefig(direcdate+"/point/"+filename+"val"+".png")
+        plt.close()
+
+    fig, ax = plt.subplots()   
+
+    plt.title("Countries covid data")
+    plt.xlabel("Date")
+    plt.ylabel("Cases")
+    ax.xaxis.set_tick_params(rotation=30, labelsize=10)
+    for i in range(len(x)):
+        print(countries[i])
+        xi=np.array(x[i])
+        yi=np.array(y[i])
+        plt.plot(xi,yi,label=countries[i])#, xdate=True, ydate=False)
+
+    plt.legend()        
+    plt.savefig(direcdate+"/line/"+filename+"val"+"line.png")
+    plt.close()
+
+    
+    if dates:
+        fig, ax = plt.subplots()   
+        plt.title("Countries covid growth")
+        plt.xlabel("Date")
+        plt.ylabel("Increase")
+        ax.xaxis.set_tick_params(rotation=30, labelsize=10)
+        for i in range(len(xgrow)):
+            print(countries[i])
+            xi=np.array(xgrow[i])
+            yi=np.array(ygrow[i])
+            plt.plot_date(xi,yi,label=countries[i], xdate=True, ydate=False)
+
+        plt.legend()        
+        plt.savefig(direcdate+"/point/"+filename+"grow"+".png")
+        plt.close()
+
+    fig, ax = plt.subplots()   
+    plt.title("Countries covid growth")
+    plt.xlabel("Date")
+    plt.ylabel("Increase")
+
+    ax.xaxis.set_tick_params(rotation=30, labelsize=10)
+    for i in range(len(xgrow)):
+        print(countries[i])
+        xi=np.array(xgrow[i])
+        yi=np.array(ygrow[i])
+        plt.plot(xi,yi,label=countries[i])#, xdate=True, ydate=False)
+    
+    plt.legend()        
+    plt.savefig(direcdate+"/line/"+filename+"grow"+"line.png")
+    plt.close()
+
+def apply_convolution(x,y,xgrow,ygrow,conv_len):
+    yconvval=[]
+    for yi in y:
+        yconva=[]
+        last_conv=0
+        for i in range(conv_len-1):
+            yconva.append(int(last_conv))
+        
+        for i in range(conv_len-1,len(yi)):
+            last_conv=sum([yi[j] for j in range(i-conv_len+1,i+1)])/conv_len
+            yconva.append(last_conv)
+
+        
+            
+        yconvval.append(yconva)
+    yconvgrow=[]
+    for yi in ygrow:
+        yconva=[]
+        last_conv=0
+        for i in range(conv_len-1):
+            yconva.append(int(last_conv))
+            
+        for i in range(conv_len-1,len(yi)):
+            last_conv=sum([yi[j] for j in range(i-conv_len+1,i+1)])/conv_len
+            yconva.append(last_conv)
+
+            
+        yconvgrow.append(yconva)
+    
+    return (yconvval,yconvgrow)
 
 def generate_graphs(countries,filename):
-    if not os.path.exists("point"):
-        os.makedirs("point")
-    if not os.path.exists("line"):
-        os.makedirs("line")
     x=[]
     y=[]
     xgrow=[]
@@ -67,73 +169,28 @@ def generate_graphs(countries,filename):
         y.append(yaux)
         xgrow.append(xgrowa)
         ygrow.append(ygrowa)
-
+    for i in range(len(xgrow)):
+        xgrow[i].pop()
+        ygrow[i].pop()
+        
+    (yconvval,yconvgrow)=apply_convolution(x,y,xgrow,ygrow,10)
+    
+    pred=[]
+    for i in range(len(yconvval)):
+        xi=x[i]
+        yi=yconvval[i]
+        yj=y[i]
+        predval=(yj[len(yj)-2]/yi[len(yi)-2])*yi[len(yi)-1]
+        pred.append({"country":countries[i],"val":predval,"date":str(xi[len(xi)-1])})
+        
     today=datetime.datetime.today()
+    file = open(str(today.day)+"_"+str(today.month)+filename+"prediction.txt", "w")
+    json.dump(pred,file)
+    file.close()    
 
-    fig, ax = plt.subplots()   
-
-    plt.title("Countries covid data")
-    plt.xlabel("Date")
-    plt.ylabel("Cases")
-    ax.xaxis.set_tick_params(rotation=30, labelsize=10)
-    for i in range(len(x)):
-        print(countries[i])
-        xi=np.array(x[i])
-        yi=np.array(y[i])
-        plt.plot_date(xi,yi,label=countries[i], xdate=True, ydate=False)
-
-    plt.legend()        
-    plt.savefig("point/"+filename+"val"+str(today.day)+"_"+str(today.month)+".png")
-    plt.close()
-
-    fig, ax = plt.subplots()   
-
-    plt.title("Countries covid data")
-    plt.xlabel("Date")
-    plt.ylabel("Cases")
-    ax.xaxis.set_tick_params(rotation=30, labelsize=10)
-    for i in range(len(x)):
-        print(countries[i])
-        xi=np.array(x[i])
-        yi=np.array(y[i])
-        plt.plot(xi,yi,label=countries[i])#, xdate=True, ydate=False)
-
-    plt.legend()        
-    plt.savefig("line/"+filename+"val"+str(today.day)+"_"+str(today.month)+"line.png")
-    plt.close()
-
-    fig, ax = plt.subplots()   
-    plt.title("Countries covid growth")
-    plt.xlabel("Date")
-    plt.ylabel("Increase")
-
-    ax.xaxis.set_tick_params(rotation=30, labelsize=10)
-    for i in range(len(xgrow)):
-        print(countries[i])
-        xi=np.array(xgrow[i])
-        yi=np.array(ygrow[i])
-        plt.plot_date(xi,yi,label=countries[i], xdate=True, ydate=False)
-
-    plt.legend()        
-    plt.savefig("point/"+filename+"grow"+str(today.day)+"_"+str(today.month)+".png")
-    plt.close()
-
-    fig, ax = plt.subplots()   
-    plt.title("Countries covid growth")
-    plt.xlabel("Date")
-    plt.ylabel("Increase")
-
-    ax.xaxis.set_tick_params(rotation=30, labelsize=10)
-    for i in range(len(xgrow)):
-        print(countries[i])
-        xi=np.array(xgrow[i])
-        yi=np.array(ygrow[i])
-        plt.plot(xi,yi,label=countries[i])#, xdate=True, ydate=False)
-
-    plt.legend()        
-    plt.savefig("line/"+filename+"grow"+str(today.day)+"_"+str(today.month)+"line.png")
-    plt.close()
-
+    draw_data(countries,filename,x,y,xgrow,ygrow,"data",True)
+    draw_data(countries,filename,x,yconvval,xgrow,yconvgrow,"conv",False)
+    
 if __name__ == "__main__":
     countries=["romania","germany","italy","hungary","spain","portugal","austria","denmark"]#,"france"]#,"united-states"]
     generate_graphs(countries,"grapheurope")
